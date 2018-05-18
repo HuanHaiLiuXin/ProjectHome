08090e122a7481d702065d217f199fede49456c5
幻海流心 <472868476@qq.com>
2018-05-18 16:59:02 +0800

    添加新项目ProxyTool

---
+++ ProxyTool/proxytool-compiler/src/main/java/proxytool/compiler/model/FieldViewBinding.java
@@ -0,0 +1,10 @@
+package proxytool.compiler.model;
+
+/**
+ * 作者:幻海流心
+ * GitHub:https://github.com/HuanHaiLiuXin
+ * 邮箱:wall0920@163.com
+ * 2018/5/18 16:48
+ */
+public class FieldViewBinding {
+}
